aef3a142b4f68a032b652949bee51ad6f1cbb637
jannisjr <joe_hannis@hotmail.com>
2023-05-12 16:38:58 +0100

    completed rspec and program for reminder challenge

--- lib/reminder.rb
+++ lib/reminder.rb
@@ -1,16 +1,18 @@
 class Reminder
-  def initialize(name)
-    fail "Please enter a name" if name == ""
-    @name = name
-    @task = ""
+  def initialize
     @task_list = []
     # ...
   end
 
   def remind_me_to(task) # task is a string
     fail "Please enter a task" if task == ""
-    @task = task
     @task_list << task
+  end
+
+  def remove_task(task)
+    if @task_list.include?(task)
+      @task_list.delete(task)
+    end
   end
 
   def remind()
--- spec/reminder_spec.rb
+++ spec/reminder_spec.rb
@@ -1,18 +1,32 @@
 require 'reminder'
 
 RSpec.describe Reminder do
-  context "takes a name, takes a task to do, and then return a string 'task, name'" do
-    it "nothing is entered for name" do
-      expect { Reminder.new("") }.to raise_error "Please enter a name"
-    end
+  context "if no task is entered raise error" do
     it "nothing is entered for task" do
-      reminder = Reminder.new("John")
+      reminder = Reminder.new
       expect { reminder.remind_me_to("") }.to raise_error "Please enter a task"
     end
-    it "takes a name, takes a task to do, and then return a string 'task, name'" do
-      reminder = Reminder.new("John")
+  end
+  context "Adding tasks to list" do
+    it "takes a task to do, and then returns a list of tasks" do
+      reminder = Reminder.new
       reminder.remind_me_to("walk the dog")
       expect(reminder.remind).to eq ["walk the dog"]
+    end
+    it "takes multiple task to do, and then returns a list of tasks" do
+      reminder = Reminder.new
+      reminder.remind_me_to("walk the dog")
+      reminder.remind_me_to("clean the house")
+      expect(reminder.remind).to eq ["walk the dog", "clean the house"]
+    end
+  end
+  context "marking tasks as completed and removing from list" do 
+    it "check to see if task is in the list, then remove it and print confirmation"do
+      reminder = Reminder.new
+      reminder.remind_me_to("walk the dog")
+      reminder.remind_me_to("clean the house")
+      reminder.remove_task("walk the dog")
+      expect(reminder.remind).to eq ["clean the house"]
     end
   end
 end
