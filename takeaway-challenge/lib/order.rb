b93ffc4d08812124f57504f6db2b120a169ca17d
jannisjr <joe_hannis@hotmail.com>
2023-05-18 18:00:57 +0100

    fixed dish, menu and order classes. Still broken receipt class

--- takeaway-challenge/lib/order.rb
+++ takeaway-challenge/lib/order.rb
@@ -2,32 +2,30 @@ require_relative 'dish'
 require_relative 'menu'
 
 class Order
-  attr_reader :order
-
+  attr_accessor :selection
   def initialize(menu)
     @menu = menu
+    @selection = []
   end
 
   def print_order
-    @order.each { puts menu.dishes }
+    @selection.each { puts menu.dishes }
   end
 
   def take_order
-    @order = []
     loop do
-      binding.irb
       puts "Select the number of the dish you'd like to order and press return. When you're done, press return again"
       order_no = gets.chomp
       break if order_no.empty?
 
       index = order_no.to_i - 1
-      if index >= 0 && index < menu.dishes.count
-        @order << menu.dishes[index]
+      if index >= 0 && index < @menu.dishes.count
+        @selection << @menu.dishes[index]
       else
         puts "Invalid dish number. Please try again."
       end
       puts "You have ordered:"
-      @order.each do |dish|
+      @selection.each do |dish|
         puts dish.item
       end
     end
--- takeaway-challenge/lib/receipt.rb
+++ takeaway-challenge/lib/receipt.rb
@@ -1,4 +1,28 @@
-require 'dish'
-require 'menu'
-require 'order'
+require_relative 'dish'
+require_relative  'menu'
+require_relative  'order'
 
+class Receipt
+  def initialize(order)
+    @order = order
+  end
+
+  def print_receipt
+    p "this is running"
+    p @order.selection#.each do |dish|
+    #   binding.irb
+    #   puts "#{@selection.menu.dish.item} £#{@selection.menu.dish.price}"
+  #   end
+  end
+end
+
+menu = Menu.new
+dish_one = Dish.new("Margharita", 10.00)
+dish_two = Dish.new("Calzone", 12.00)
+menu.add_dish(dish_one)
+menu.add_dish(dish_two)
+order = Order.new(menu)
+order.take_order
+p order.selection#selection is blank outside of the program. Need to find out why
+receipt = Receipt.new(order)
+receipt.print_receipt
