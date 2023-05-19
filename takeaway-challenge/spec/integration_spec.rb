bd0ecdf8fadf0c7ea1903869d45538576d634c84
jannisjr <joe_hannis@hotmail.com>
2023-05-19 16:32:28 +0100

    cleaned up integration Rspec

--- takeaway-challenge/spec/integration_spec.rb
+++ takeaway-challenge/spec/integration_spec.rb
@@ -7,19 +7,19 @@ RSpec.describe Order do
   let(:menu) { Menu.new }
   let(:dish_one) { Dish.new("Margharita", 10.00) }
   let(:dish_two) { Dish.new("Calzone", 12.00) }
+  let(:io) { double(:io) }
+  let(:order) { Order.new(menu, io) }
+  let(:receipt) { Receipt.new(order, io) }
 
   context "integration" do
 
-    it "populates a menu and prints it to the screen, then asks for an order and returns the order so far with multiple items. An itemised receipt is then generated" do
+    it "Populates a menu and prints it to the screen, then asks for an order and returns the order so far with multiple items. An itemised receipt is then generated" do
       menu.add_dish(dish_one)
       menu.add_dish(dish_two)
 
-      io = double(:io)
       allow(io).to receive(:puts) # Stubbing puts to avoid unnecessary output in the test
       allow(io).to receive(:gets).and_return("1", "2", "1", "1", "") # Stubbing user input to select dish 1 and then provide an empty input to exit the loop
       
-      order = Order.new(menu, io)
-
       expect(io).to receive(:puts).with("Select the number of the dish you'd like to order and press return. When you're done, press return again")
       expect(io).to receive(:puts).with("You have ordered:")
       expect(io).to receive(:puts).with("Margharita")
@@ -28,38 +28,12 @@ RSpec.describe Order do
       expect(io).to receive(:puts).with("Margharita")
       order.take_order
 
-      receipt = Receipt.new(order, io)
       expect(io).to receive(:puts).with("Margharita £10.00")
       expect(io).to receive(:puts).with("Calzone £12.00")
       expect(io).to receive(:puts).with("Margharita £10.00")
       expect(io).to receive(:puts).with("Margharita £10.00")
       expect(io).to receive(:puts).with("GRAND TOTAL: £42.00")
       receipt.print_receipt
-
-
     end
   end
 end
-
-
-
-
-  #   it "populates a menu and rints it to screen, then asks for an order and stores user input" do
-  #     io = double(:io)
-  #     allow(io).to receive(:puts) # Stubbing puts to avoid unnecessary output in the test
-  #     allow(io).to receive(:gets).and_return("1", "2", "1", "1", "") # Stubbing user input to select dish 1 and then provide an empty input to exit the loop
-      
-
-  #     expect(io).to receive(:puts).with("Select the number of the dish you'd like to order and press return. When you're done, press return again")
-  #     expect(io).to receive(:puts).with("You have ordered:")
-  #     expect(io).to receive(:puts).with("Margharita")
-  #     expect(io).to receive(:puts).with("Calzone")
-  #     expect(io).to receive(:puts).with("Margharita")
-  #     expect(io).to receive(:puts).with("Margharita")
-      
-  #     order.take_order
-  #     expect(io).to receive(:puts).with("Margharita")
-  #     receipt = Receipt.new(order, io)
-  #   end
-  # end
-#end
